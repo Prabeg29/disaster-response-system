14c5df40dacae4a90b6e6d0505943bde34f5fb0a
Prabeg Shakya <shakyaprabeg@gmail.com>
2026-06-11 10:46:36 +1000

    Add findById, update and delete to EvacuationZoneDao interface

--- src/main/java/com/coit20258/drs/dao/EvacuationZoneDao.java
+++ src/main/java/com/coit20258/drs/dao/EvacuationZoneDao.java
@@ -1,6 +1,7 @@
 package com.coit20258.drs.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.coit20258.drs.model.EvacuationZone;
 
@@ -12,5 +13,11 @@ public interface EvacuationZoneDao {
 
     List<EvacuationZone> findByReportId(int reportId);
 
+    Optional<EvacuationZone> findById(int id);
+
+    boolean update(EvacuationZone zone);
+
     boolean updateOccupancy(int id, int currentOccupancy, String status);
+
+    boolean delete(int id);
 }
